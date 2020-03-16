34b1445e847071b0e6690a1fdd9aa3d15d48665a
ashim dahal <ashim44@gmail.com>
2020-03-16 17:24:09 -0500

    Create bindable for view model

---
+++ AppStoreApplication/Album View Controller/Bindable.swift
@@ -0,0 +1,29 @@
+//
+//  Bindable.swift
+//  AppStoreApplication
+//
+//  Created by ashim Dahal on 3/16/20.
+//  Copyright © 2020 ashim Dahal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Bindable {
+    /// defined type associated that is represented by view model
+    associatedtype ViewModel
+    /// associated value of its corresponding ViewModel
+    var viewModel: ViewModel {get set}
+    
+    /// when executed it will bind all define element between the class that use it and view model object
+    func bindViewModel()
+}
+
+extension Bindable where Self: UIViewController {
+    
+    mutating func setViewModel(to model: Self.ViewModel) {
+        viewModel = model
+        loadViewIfNeeded()
+        bindViewModel()
+    }
+}
