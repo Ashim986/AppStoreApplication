d696bb0c6fff361c8491936cbd737f84e4adae84
ashim dahal <ashim44@gmail.com>
2020-03-20 18:22:53 -0500

    Update Response.swift

--- AppStoreApplication/Data/Response.swift
+++ AppStoreApplication/Data/Response.swift
@@ -10,30 +10,14 @@ import Foundation
 
 struct Response <T: Decodable>: Decodable {
     let feed: Feed<T>?
-    //let errors: [ResponseErrors]
     
     init(from decoder: Decoder) throws {
         let map = try decoder.container(keyedBy: CodingKeys.self)
         feed = try? map.decode(Feed.self, forKey: .feed)
-        //errors = try map.decode([ResponseErrors].self, forKey: .errors)
     }
        
     enum CodingKeys: String, CodingKey {
         case feed = "feed"
-        case errors = "Errors"
-    }
-}
-
-struct ResponseErrors: Decodable, Error {
-    
-    let errorCode: String
-    let text: String
-    let description: String
-    
-    enum CodingKeys: String, CodingKey {
-        case errorCode = "ErrorCode"
-        case text = "Text"
-        case description = "Description"
     }
 }
 
